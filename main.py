6e0467c904bb1c69db9434de9717a057203a4171
louislombard <louis.raphael.lombard@gmail.com>
2021-08-26 11:24:50 +0200

    init

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import json
 import sys
 from instaManager.instaManager import InstaManager
 
+# const
 PATH_TO_DRIVER = "./chromedriver"
 CREDENTIALS_FILENAME = os.path.join(os.path.dirname(__file__), 'credentials.json')
 
+# get cred info
 try:
     f = open(CREDENTIALS_FILENAME)
     my_cred = json.load(f)
@@ -19,4 +21,4 @@ except Exception as bad_except:
 if __name__ == '__main__':
     my_insta_manager = InstaManager(PATH_TO_DRIVER, insta_username, insta_password)
     my_insta_manager.connect()
-    my_insta_manager.find_and_comment_post_by_tag("indiemusic", percentage_like=0.3, percentage_comment=0.2)
+    my_insta_manager.find_and_comment_post_by_tag("indiemusic", percentage_like=0.2, percentage_comment=0.2)
